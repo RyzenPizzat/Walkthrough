81468543a6e21d6efad87f79d52ddc83c5819353
RyzenPizzat <kamal.achmad30@gmail.com>
2025-08-09 20:52:13 +0800

    FINAL CRUD

--- Project/perpustakaan.cpp
+++ Project/perpustakaan.cpp
@@ -7,6 +7,7 @@ void sapa();
 int proses(string x);
 int tampilkan();
 int tambah_data();
+int hapus_data();
 int main()
 {
     sapa();
@@ -19,34 +20,44 @@ void sapa()
     cout << "1. Tampilkan Data." << endl;
     cout << "2. Tambah Data" << endl;
     cout << "3. Hapus Data" << endl;
+    cout << "4. Keluar" << endl;
+    cout << "=============================================" << endl;
     cout << "Pilih Menu : ";
     cin >> a;
     cin.ignore();
     proses(a);
 }
-int proses(string x)
-{
-    if (x == "1")
-    {
-        cout << "User Memilih 1" << endl;
-        tampilkan();
+int proses(string x) {
+    if (x == "1") {
+        if (buku.size() > 0) {
+            tampilkan();
+        } else {
+            cout << "Tidak ada data buku." << endl;
+            sapa();
+        }
     }
-    else if (x == "2")
-    {
-        cout << "User Memilih 2" << endl;
+    else if (x == "2") {
         tambah_data();
     }
-    else if (x == "3")
-    {
-        cout << "User Memilih 3" << endl;
+    else if (x == "3") {
+        if (buku.size() > 0) {
+            hapus_data();
+        } else {
+            cout << "Tidak ada data buku untuk dihapus." << endl;
+            sapa();
+        }
     }
-    else
-    {
-        cout << "User salah Memilih!" << endl;
+    else if (x == "4") {
+        cout << "Terima kasih telah menggunakan Perpustakaan Majalengka." << endl;
+        return 0;
+    }
+    else {
+        cout << "Pilihan tidak valid, silakan coba lagi." << endl;
         sapa();
     }
     return 0;
 }
+
 int tambah_data()
 {
     string t_buku;
@@ -71,5 +82,22 @@ int tampilkan()
         cout << "Buku Ke : " << i + 1 << " : " << buku[i] << " - " << buku[i + 1] << endl;
         cout << i << endl;
     }
+    sapa();
+    return 0;
+}
+
+int hapus_data()
+{
+    int hapus;
+    cout << "Masukkan Nomor Buku yang ingin dihapus : ";
+    cin >> hapus;
+    if (hapus >= 0) {
+        buku.erase(buku.begin() + hapus - 1);
+        buku.erase(buku.begin() + (hapus ));
+    }
+    else {
+        cout << "Nomor Buku tidak valid!" << endl;
+    }
+    sapa();
     return 0;
 }
